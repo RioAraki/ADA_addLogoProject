8ac05acdef55173f5bb7978ee94ad4d51edbb53c
RioAraki <yueee.li@mail.utoronto.ca>
2016-10-22 16:48:14 -0400

    update function and index in order to see the canvas in png

--- rio/function.js
+++ rio/function.js
@@ -1,29 +1,24 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-var imageObj1 = new Image();
-var imageObj2 = new Image();
+var logo = new Image();
+var background = new Image();
 
-function addImage() {
-  context.drawImage(imageObj1, 69, 50);
+/////////////////////////// add logo functions
+function addLogo() {
+  context.drawImage(logo,0,0);
 }
 
-function add2Image() {
-    context.drawImage(imageObj2, 0, 280);
-}
-imageObj1.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
-imageObj2.src = 'http://canyongatedental.com/wp-content/uploads/2016/09/ADA-Logo-650x230.png';
-
-function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL();
-    link.download = filename;
+function addBackground() {
+  context.drawImage(background,0,0);
 }
 
-///////////////////////////
+logo.src = "https://rioaraki.github.io/addLogo/logo.svg";
+background.src = "https://rioaraki.github.io/addLogo/background.svg";
+/////////////////////////// picture modification, change pictures that are too small/large to right proportion
 $('#pic1').change(function() {
   var fr = new FileReader;
 
   fr.onload = function() {
-    //var img = canvas.toDataURL("image/png");
     var img = new Image();
     img.onload = function() {
         var sourceX = img.width/2 - canvas.width/2;
@@ -36,18 +31,19 @@ $('#pic1').change(function() {
         var destY = canvas.height / 2 - destHeight / 2;
         if(img.width < canvas.width || img.height < canvas.height ){
             context.drawImage(img,0,0,500,500);
-        }else{context.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);};
-        
+        }else{
+          context.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+        };
     };
     img.src = fr.result;
   };
   fr.readAsDataURL(this.files[0]);
 });
 
-////////////////////////////
-var downloadImg = canvas.toDataURL("image/png");
-download = document.getElementById('canvas-download');
-download.addEventListener('click', function() {
-    var data = downloadImg;
-    download.href = data;
-}, false);
+//////////////////////////// picture download, download the canvas as png
+
+function generateImage() {
+  var dataURL = canvas.toDataURL();
+  document.getElementById('canvasImg').src = dataURL;
+  console.log(dataURL);
+}
