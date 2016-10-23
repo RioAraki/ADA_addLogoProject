c06b3aea593fcf87e71b94642772f8172c182cbd
HesterG <hestergong@gmail.com>
2016-10-23 12:32:49 +0800

    Modified function

--- rio/function.js
+++ rio/function.js
@@ -21,19 +21,51 @@ $('#pic1').change(function() {
   fr.onload = function() {
     var img = new Image();
     img.onload = function() {
-        var sourceX = img.width/2 - canvas.width/2;
-        var sourceY = img.height/2 - canvas.height/2;
-        var sourceWidth = canvas.width;
-        var sourceHeight = canvas.height;
-        var destWidth = sourceWidth;
-        var destHeight = sourceHeight;
-        var destX = canvas.width / 2  - destWidth / 2;
-        var destY = canvas.height / 2 - destHeight / 2;
-        if(img.width < canvas.width || img.height < canvas.height ){
-            context.drawImage(img,0,0,500,500);
+        var shorter_side = Math.min(img.width, img.height);
+        console.log(shorter_side); //800
+        var r = shorter_side/canvas.width; 
+        console.log(r); //0.625
+        console.log(img.width); //1280
+        console.log(img.height); //800
+        old_width = img.width;
+        old_height = img.height;
+        new_width = img.width * r;
+        new_height = img.height * r;
+        console.log(img.width); //800
+        console.log(img.height); //500
+        console.log(canvas.width); //500
+        if(old_width > old_height){
+            var sourceX = (old_width - canvas.width*r)/2; 
+            var sourceY = 0;
+            var sourceWidth = canvas.width*r;
+            var sourceHeight = canvas.height*r;
+            var destWidth = canvas.width;
+            var destHeight = canvas.height;         
+            var destX = 0;
+            var destY = 0;
         }else{
-          context.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-        };
+            var sourceX = 0; 
+            var sourceY = (old_height - canvas.height*r)/2;
+            var sourceWidth = canvas.width*r;
+            var sourceHeight = canvas.height*r;
+            var destWidth = canvas.width;
+            var destHeight = canvas.height;            
+            var destX = 0;
+            var destY = 0;  
+        }
+        
+        console.log(sourceX);//240
+        console.log(sourceY);//150
+        console.log(sourceWidth);//500
+        console.log(sourceHeight);//500
+        console.log(destX);//0
+        console.log(destY);//0
+        console.log(destWidth);//500
+        console.log(destHeight);//500
+        context.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+        //if(img.width < canvas.width || img.height < canvas.height ){
+        //    context.drawImage(img,0,0,500,500);
+        //}else{context.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);};
     };
     img.src = fr.result;
   };
